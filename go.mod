3a770af4c823dba8ef3065056c183c36f1f8016d
uperbilite <uperbilite@gmail.com>
2023-07-23 00:16:58 +0800

    ADD: connection pool in client.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module simple-rpc
 go 1.19
 
 require (
+	github.com/silenceper/pool v1.0.0
 	github.com/stretchr/testify v1.8.4
 	go.etcd.io/etcd/client/v3 v3.5.9
 	google.golang.org/grpc v1.56.2
@@ -15,7 +16,9 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.4.2 // indirect
 	go.etcd.io/etcd/api/v3 v3.5.9 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.5.9 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
--- proxy/client.go
+++ proxy/client.go
@@ -2,21 +2,37 @@ package simple_rpc
 
 import (
 	"context"
+	"github.com/silenceper/pool"
 	"net"
 	"simple-rpc/proxy/message"
 	"simple-rpc/proxy/serialize"
 	"simple-rpc/proxy/serialize/json"
+	"time"
 )
 
 type Client struct {
 	addr       string
+	connPool   pool.Pool
 	serializer serialize.Serializer
 }
 
 func NewClient(addr string, opts ...ClientOption) *Client {
+	poolConfig := &pool.Config{
+		InitialCap:  5,
+		MaxIdle:     20,
+		MaxCap:      30,
+		Factory:     func() (interface{}, error) { return net.Dial("tcp", addr) },
+		Close:       func(v interface{}) error { return v.(net.Conn).Close() },
+		IdleTimeout: time.Minute,
+	}
+	connPool, err := pool.NewChannelPool(poolConfig)
+	if err != nil {
+		return nil
+	}
 	res := &Client{
 		addr:       addr,
 		serializer: &json.Serializer{},
+		connPool:   connPool,
 	}
 	for _, opt := range opts {
 		opt(res)
@@ -33,15 +49,16 @@ func ClientWithSerializer(serializer serialize.Serializer) ClientOption {
 }
 
 func (c *Client) Invoke(ctx context.Context, req *message.Request) (*message.Response, error) {
-	conn, err := net.Dial("tcp", c.addr)
+	conn, err := c.connPool.Get()
+	defer c.connPool.Put(conn)
 	if err != nil {
 		return nil, err
 	}
 
 	data := message.EncodeReq(req)
-	Write(conn, data)
+	Write(conn.(net.Conn), data)
 
-	data = Read(conn)
+	data = Read(conn.(net.Conn))
 	res := message.DecodeResp(data)
 
 	return res, nil
